e5c0fd0e55423a9dbf615e177c8febe24b718842
kanzakirei <kanzakirei@aqua.gmail.com>
2026-07-25 15:14:25 +0900

    繝・・繝ｭ繧､

--- js/base.js
+++ js/base.js
@@ -1,9 +1,21 @@
 document.documentElement.style.overflow = "hidden";
 
+document.addEventListener('DOMContentLoaded', () => {
+  setupSplitTextAnimation();
+});
+
 window.onload = function () {
   document.getElementById("loading").classList.toggle("fadeOut");
   document.documentElement.style.overflow = "visible";
+  playSplitTextAnimation();
 }
+
+// デバッグ用
+// setTimeout(() => {
+//   document.getElementById("loading").classList.toggle("fadeOut");
+//   document.documentElement.style.overflow = "visible";
+//   playSplitTextAnimation();
+// }, 1000); // 3000ミリ秒後に発火
 
 function request(_endPoint, _json, _onSuccessed = null, _onErrored = null) {
   const headers = new Headers();
@@ -50,4 +62,43 @@ function onClickedNavigationButton(element) {
   else {
     navigation.classList.add("fadeIn");
   }
+}
+
+function setupSplitTextAnimation() {
+  const targets = document.querySelectorAll(".splitTextAnimation");
+  
+  targets.forEach((target) => {
+    const textContent = target.innerText;
+    target.textContent = '';
+
+    textContent.split('').forEach((char, index) => {
+      if (char === '\n') {
+        const br = document.createElement('br');
+        target.appendChild(br);
+        return; 
+      }
+
+      const span = document.createElement('span');
+      span.innerText = char === ' ' ? '\u00A0' : char; 
+      span.style.setProperty('--delay', index); 
+      span.classList.add('char');
+      target.appendChild(span);
+    });
+  });
+}
+
+function playSplitTextAnimation() {
+  const targets = document.querySelectorAll(".splitTextAnimation");
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-animated'); 
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    rootMargin: "0px 0px -10% 0px"
+  });
+
+  targets.forEach((target) => observer.observe(target));
 }
